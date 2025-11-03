246b3ed009e0e7722c854588a5d06f8129e90e95
Lee-chaerin <81899084+Lee-chaerin@users.noreply.github.com>
2025-11-03 17:36:17 +0900

    feat: 당첨 결과 계산하기 기능 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -13,6 +13,20 @@ class Lotto {
     return this.#numbers;
   }
 
+  checkNumbers(winningNumber, bonusNumber) {
+    const matchCount = this.#numbers.filter((number) =>
+      winningNumber.includes(number)
+    ).length;
+    const bonusMatch = this.#numbers.includes(bonusNumber);
+
+    if (matchCount === 6) return 1;
+    if (matchCount === 5 && bonusMatch) return 2;
+    if (matchCount === 5) return 3;
+    if (matchCount === 4) return 4;
+    if (matchCount === 3) return 5;
+    return 0;
+  }
+
   #validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
