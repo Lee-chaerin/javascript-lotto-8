488d5d60a84c3a0b43d6c5e9d974e203d0e66634
Lee-chaerin <81899084+Lee-chaerin@users.noreply.github.com>
2025-11-03 18:42:17 +0900

    refactor(lottoOutput.js): 파일명, 함수명 변경
    - lottoOutput.js -> lottoPrint.js
    - lottoOutput() -> lottoPrint()

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { priceInput } from "./input/priceInput.js";
 import { bonusInput, winningInput } from "./input/winningNumberInput.js";
-import { lottoOutput } from "./output/lottoOutput.js";
+import { lottoPrint } from "./output/lottoPrint.js";
 import { resultOutput } from "./output/resultOutput.js";
 
 class App {
   async run() {
     const [price, lottoCount] = await priceInput(); //1. 구입
 
-    const lottoArray = lottoOutput(lottoCount);
+    const lottoArray = lottoPrint(lottoCount); //2. 구매한 로또 반환 및 출력
 
     const winningNumber = await winningInput();
     const bonusNumber = await bonusInput(winningNumber);
--- src/output/lottoPrint.js
+++ src/output/lottoPrint.js
@@ -2,11 +2,12 @@ import { Console } from "@woowacourse/mission-utils";
 import { OUTPUT_MESSAGE } from "../constant/output.js";
 import { getLotto } from "../lotto/getLotto.js";
 
-export function lottoOutput(lottoCount) {
+export function lottoPrint(lottoCount) {
   Console.print("");
   Console.print(lottoCount + OUTPUT_MESSAGE.LOTTO_COUNT);
 
   const lottoArray = getLotto(lottoCount);
+  
   for (const lotto of lottoArray) {
     Console.print(lotto.getNumbers());
   }
